ea9a1381ecb903b499f89850cb49dbe47ee3bb68
Niruban-A <nirubanchipriyan1453@gmail.com>
2025-10-24 09:37:07 +0530

    feat:sending data using url)

--- sending_data/lib/main.dart
+++ sending_data/lib/main.dart
@@ -3,33 +3,38 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void main(){
-  runApp(MaterialApp(home: Displaydata(),));
+void main() {
+  runApp(MaterialApp(home: Displaydata()));
 }
-class Album{
+
+class Album {
   int id;
   String title;
   Album({required this.id, required this.title});
-  factory Album.fromjson(Map<String,dynamic> json){
-    return switch (json){
-      {
-        "id":int id,"title":String title
-      }=> Album(id: id,title: title),
-      _=>throw FormatException("the format does not correct")
+  factory Album.fromjson(Map<String, dynamic> json) {
+    return switch (json) {
+      {"id": int id, "title": String title} => Album(id: id, title: title),
+      _ => throw FormatException("the format does not correct"),
     };
   }
 }
-Future <Album> fetchdata (String title)async{
-  final response= await http.post(Uri.parse('https://jsonplaceholder.typicode.com/albums') ,
-  headers: <String, String>{
+
+Future<Album> fetchdata(String title) async {
+  final response = await http.post(
+    Uri.parse('https://jsonplaceholder.typicode.com/albums'),
+    headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
-    body:jsonEncode(<String,String>{
-      "title":title
-    }) );
-    return Album.fromjson(jsonDecode(response.body)as Map<String,dynamic>);
-
+    body: jsonEncode(<String, String>{"title": title}),
+  );
+  if (response.statusCode==201){
+  return Album.fromjson(jsonDecode(response.body) as Map<String, dynamic>);
+  }
+  else{
+    throw Exception("failed to create album");
+  }
 }
+
 class Displaydata extends StatefulWidget {
   const Displaydata({super.key});
 
@@ -38,32 +43,58 @@ class Displaydata extends StatefulWidget {
 }
 
 class _DisplaydataState extends State<Displaydata> {
-   TextEditingController mycontroller= TextEditingController();
+  TextEditingController mycontroller = TextEditingController();
 
-   
   @override
   Widget build(BuildContext context) {
-    return Center(child: TextField(controller: mycontroller,onSubmitted: (value) => {Navigator.push(context, MaterialPageRoute(builder:  (context) => GetInput(),))},
-    ),);
+    return Scaffold(
+      body: Center(
+        child: SizedBox(height: 100,width: 600,
+          child: TextField(decoration:InputDecoration(border: OutlineInputBorder()),
+            controller: mycontroller,
+            onSubmitted: (value) => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GetInput(title:mycontroller.text)),
+              ),
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
+
 class GetInput extends StatelessWidget {
-  const GetInput({super.key});
+  const GetInput({super.key,required this.title});
+  final title;
 
   @override
   Widget build(BuildContext context) {
-      Future<Album>? myalbum;
-        return Scaffold(body:Center(child:Column(children: [FutureBuilder(future: myalbum, builder: (context, snapshot) {
-          if(snapshot.hasData){
-            return Text("$snapshot");
-          }
-          else if(snapshot.hasData){
-            return Text("${snapshot.error}");
-          }
-          else{
-            return CircularProgressIndicator();
-          }
-        },)],),) ,);
-
+    Future<Album>? myalbum;
+    myalbum=fetchdata(title);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FutureBuilder(
+              future: myalbum,
+              builder: (context, snapshot) {
+                
+                if (snapshot.hasData) {
+                  
+                  return Text("title:${snapshot.data!.title}");
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                } else {
+                  return CircularProgressIndicator();
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
